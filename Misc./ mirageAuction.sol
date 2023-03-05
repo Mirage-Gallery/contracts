f93b8652f1b8c3cab3635f96299c80d4bc738f0e
augustfr <68298953+augustfr@users.noreply.github.com>
2023-03-05 15:08:15 -0800

    add auction contract

---
+++ Misc./ mirageAuction.sol
@@ -0,0 +1,397 @@
+/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMNOOXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXO0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMk,'lKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc.,kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMNo...cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc...lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMK:....cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc....:0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMk,.....cKMMMMMMMMMMMMMMMMMMMMMMMMMMWKc.....,kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNd.......cKWMMMMMMMMMMMMMMMMMMMMMMMKkc.......oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMKc........cKMMMMMMMMMMMMMMMMMMMMMMKc'........cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMO,.........cKMMMMMMMMMMMMMMMMMMMMKc..........,OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMWd'..........cKWMMMMMMMMMMMMMMMMMKl............dWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMXc...;;.......cKMMMMMMMMMMMMMMMMKc...'co,......cXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMO;..,x0:.......cKMMMMMMMMMMMMMMKl...cxKKc......;OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMWd'..;0W0:.......cKWMMMMMMMMMMMKc...cKWMNo......'xWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMXl...lXMW0:.......c0WMMMMMMMMMKl...cKMMMWx'......lXXkxxddddoddddxxxkO0KXXNWWWMMMMMMMMMMM
+MMMMMMMMMMMM0;...dWMMW0:.......c0WMMMMMMMKl...cKMMMMMO;......;0k,.'''',,''.......'',;::cxXMMMMMMMMMM
+MMMMMMMMMMMWx'..,kMMMMW0:.......c0WMMMMMKl...cKMMMMWWKc......'xXOO00KKKKK00Okdoc,'......cKMMMMMMMMMM
+MMMMMMMMMMMXl...:0MMMMMW0:.......:0WMMMKl...cKMMWKkdxKd.......oNMMMMMMMMMMMMMMMWXOd:'...lXMMMMMMMMMM
+MMMMMMMMMMM0:...lXMMMMMMWO:.......:0MMKl...cKMWKo,..;0k,......:KMMMMMMMMMMMMMMMMMMWNOc'.oNMMMMMMMMMM
+MMMMMMMMMMWx'..'dWMMMMMMMW0:.......:0Kl...cKMNk;....,k0:......,kMMMMMMMMMMMMMMMMMMMMMXl'oNMMMMMMMMMM
+MMMMMMMMMMNl...,OMMMMMMMMMW0:.......;;...cKWXo'......dKl.......oNMMMMMMMMMMMMMMMMMMMMM0:dWMMMMMMMMMM
+MMMMMMMMMM0:...:KMMMMMMMMMMW0c..........lKMNo'.......oXd'......cKMMMMMMMMMMMMMMMMMMMMMNKXMMMMMMMMMMM
+MMMMMMMMMWk,...lXMMMMMMMMMMMWKc........cKMWx,.......,kWk,......,OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMNo...'dWMMMMMMMMMMMMMKl......lKMMK:........:KMK:.......dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMWO;...'xWMMMMMMMMMMMMMMXl'...lKMMWx'........lXMXl.......;OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMNKOOd;.....;dkO0NMMMMMMMMMMMXo''lXMMMNo.........lNW0:........,lxkO0XWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMN0kkkkkkkkkkxxkOXWMMMMMMMMMMMN0ONMMMMNo.........lXWXOkkkkxxxxxxxxxk0WMXOkkkkkkkkkkkkkkkkkOXMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo.........:0MMMMMMMMMMMMMMMMMMMMN0OOxl,........,lxO0NMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx'........'xWMMMMMMMMMMMMMMMMMMMMMMMMNd'......'dNMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0:.........:KMMMMMMMMMMMMMMMMMMMMMMMMMk,......'xWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd'.........oXMMMMMMMMMMMMMMMMMMMMMMMMO,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXo.........'oXMMMMMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXo'.........c0WMMMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx;.........,dXWMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKo;'........;d0NMMMMMMMMMMMMMMMMMO,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKOxc'.......':dOKNWMMMMMMMMMMMWk,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0xl;'.......,:ldxkO00KK00Od:.......;OMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0kdl:;''.......''''''....',;cox0NMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0OkxxddddddddxxkkO0XNWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+// SPDX-License-Identifier: MIT
+// Contract authored by August Rosedale (@augustfr)
+// https://miragegallery.ai
+
+// This is a modifed version of an auction contact from Foundation
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+interface IERC721 {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint tokenId
+    ) external;
+
+    function transferFrom(
+        address,
+        address,
+        uint
+    ) external;
+}
+
+/// @param auctionId The already listed auctionId for this NFT.
+error NFTMarketReserveAuction_Already_Listed(uint256 auctionId);
+/// @param minAmount The minimum amount that must be bid in order for it to be accepted.
+error NFTMarketReserveAuction_Bid_Must_Be_At_Least_Min_Amount(uint256 minAmount);
+/// @param reservePrice The current reserve price.
+error NFTMarketReserveAuction_Cannot_Bid_Lower_Than_Reserve_Price(uint256 reservePrice);
+/// @param endTime The timestamp at which the auction had ended.
+error NFTMarketReserveAuction_Cannot_Bid_On_Ended_Auction(uint256 endTime);
+error NFTMarketReserveAuction_Cannot_Bid_On_Nonexistent_Auction();
+error NFTMarketReserveAuction_Cannot_Finalize_Already_Settled_Auction();
+/// @param endTime The timestamp at which the auction will end.
+error NFTMarketReserveAuction_Cannot_Finalize_Auction_In_Progress(uint256 endTime);
+error NFTMarketReserveAuction_Cannot_Rebid_Over_Outstanding_Bid();
+/// @param maxDuration The maximum configuration for a duration of the auction, in seconds.
+error NFTMarketReserveAuction_Exceeds_Max_Duration(uint256 maxDuration);
+/// @param extensionDuration The extension duration, in seconds.
+error NFTMarketReserveAuction_Less_Than_Extension_Duration(uint256 extensionDuration);
+/// @param seller The current owner of the NFT.
+error NFTMarketReserveAuction_Not_Matching_Seller(address seller);
+error NFTMarketReserveAuction_Too_Much_Value_Provided();
+
+contract mirageAuction is Ownable, ReentrancyGuard {
+
+    struct ReserveAuctionStorage {
+        address nftContract;
+        uint256 tokenId;
+        address payable payee1;
+        uint256 payee1Amount;
+        address payable payee2;
+        address creator;
+        uint256 endTime;
+        address payable bidder;
+        uint256 amount;
+        uint256 numBids;
+        bool finalized;
+    }
+
+    struct ReserveAuction {
+      address nftContract;
+      uint256 tokenId;
+      address payable payee1;
+      uint256 payee1Amount;
+      address payable payee2;
+      address creator;
+      uint256 duration;
+      uint256 extensionDuration;
+      uint256 endTime;
+      address payable bidder;
+      uint256 amount;
+      uint256 numBids;
+      bool finalized;
+    }
+
+    uint256 private DURATION;
+
+    uint256 private constant EXTENSION_DURATION = 10 minutes;
+
+    uint256 private constant MAX_MAX_DURATION = 1000 days;
+
+    uint256 private nextAuctionId = 1;
+
+    uint256 constant BASIS_POINTS = 10000;
+
+    uint256 constant MIN_PERCENT_INCREMENT_DENOMINATOR = BASIS_POINTS / 1000;
+
+    mapping(address => mapping(uint256 => uint256)) private nftContractToTokenIdToAuctionId;
+    mapping(uint256 => ReserveAuctionStorage) private auctionIdToAuction;
+
+    event ReserveAuctionCreated(
+        address payee1,
+        uint256 payee1Percentage,
+        address payee2,
+        address creator,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        uint256 duration,
+        uint256 extensionDuration,
+        uint256 reservePrice,
+        uint256 auctionId
+    );
+
+    event ReserveAuctionFinalized(
+        uint256 indexed auctionId,
+        address payee1,
+        address payee2,
+        address creator,
+        address indexed bidder
+    );
+
+    struct Bid {
+      address[] bidder;
+      uint256[] amount;
+      uint256[] timestamp;
+    }
+
+    mapping(uint256 => Bid) bids;
+    mapping(uint256 => address[]) bidders;
+
+    event ReserveAuctionInvalidated(uint256 indexed auctionId);
+    event ReserveAuctionBidPlaced(uint256 indexed auctionId, address indexed bidder, uint256 amount, uint256 endTime);
+  
+    constructor(uint256 duration) {
+      if (duration > MAX_MAX_DURATION) {
+          revert NFTMarketReserveAuction_Exceeds_Max_Duration(MAX_MAX_DURATION);
+        }
+        if (duration < EXTENSION_DURATION) {
+          revert NFTMarketReserveAuction_Less_Than_Extension_Duration(EXTENSION_DURATION);
+        } 
+        
+        DURATION = duration; // duration in seconds
+      }
+
+    function _getNextAndIncrementAuctionId() internal returns (uint256) {
+      unchecked {
+        return nextAuctionId++;
+      }
+    }
+
+    function getCurrentAuctionId() public view returns (uint256) {
+      return nextAuctionId - 1;
+    }
+    
+    function updateDuration(uint256 newDuration) external onlyOwner {
+      if (newDuration > MAX_MAX_DURATION) {
+          revert NFTMarketReserveAuction_Exceeds_Max_Duration(MAX_MAX_DURATION);
+        }
+        if (newDuration < EXTENSION_DURATION) {
+          revert NFTMarketReserveAuction_Less_Than_Extension_Duration(EXTENSION_DURATION);
+        } 
+      DURATION = newDuration;
+    }
+
+    function createAuction(address nftContract, uint256 tokenId, uint256 reservePrice, address payee1, uint256 payee1Amount, address payee2) external onlyOwner {
+        uint256 auctionId = _getNextAndIncrementAuctionId();
+           
+
+        // If the `msg.sender` is not the owner of the NFT, transferring into escrow should fail.
+        _transferToEscrow(nftContract, tokenId);
+
+        // This check must be after _transferToEscrow in case auto-settle was required
+        if (nftContractToTokenIdToAuctionId[nftContract][tokenId] != 0) {
+        revert NFTMarketReserveAuction_Already_Listed(nftContractToTokenIdToAuctionId[nftContract][tokenId]);
+        }
+
+        // Store the auction details
+        nftContractToTokenIdToAuctionId[nftContract][tokenId] = auctionId;
+        ReserveAuctionStorage storage auction = auctionIdToAuction[auctionId];
+        auction.nftContract = nftContract;
+        auction.tokenId = tokenId;
+        auction.payee1 = payable(payee1);
+        auction.payee1Amount = payee1Amount;
+        auction.payee2 = payable(payee2);
+        auction.creator = msg.sender;
+        auction.amount = reservePrice;
+        auction.endTime = block.timestamp + DURATION;
+        auction.numBids = 0;
+        auction.finalized = false;
+
+        emit ReserveAuctionCreated(payee1, payee1Amount, payee2, msg.sender, nftContract, tokenId, DURATION, EXTENSION_DURATION, reservePrice, auctionId);
+    }
+
+    function getReserveAuction(uint256 auctionId) external view returns (ReserveAuction memory auction) {
+      ReserveAuctionStorage storage auctionStorage = auctionIdToAuction[auctionId];
+      auction = ReserveAuction(
+        auctionStorage.nftContract,
+        auctionStorage.tokenId,
+        auctionStorage.payee1,
+        auctionStorage.payee1Amount,
+        auctionStorage.payee2,
+        auctionStorage.creator,
+        DURATION,
+        EXTENSION_DURATION,
+        auctionStorage.endTime,
+        auctionStorage.bidder,
+        auctionStorage.amount,
+        auctionStorage.numBids,
+        auctionStorage.finalized
+      );
+    }
+
+    function _transferToEscrow(address nftContract, uint256 tokenId) internal virtual {
+      uint256 auctionId = nftContractToTokenIdToAuctionId[nftContract][tokenId];
+      if (auctionId == 0) {
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+        return;
+      }
+    }
+
+    function _transferFromEscrow(address nftContract, uint256 tokenId, address recipient, address authorizeSeller) internal virtual {
+      uint256 auctionId = nftContractToTokenIdToAuctionId[nftContract][tokenId];
+      ReserveAuctionStorage memory auction = auctionIdToAuction[auctionId];
+      if (!auction.finalized) {
+        if (auction.numBids == 0) {
+          // The auction has not received any bids yet so it may be invalided.
+
+          if (authorizeSeller != address(0)) {
+            // The account trying to transfer the NFT is not the current owner.
+            revert NFTMarketReserveAuction_Not_Matching_Seller(auction.payee1);
+          }
+
+          emit ReserveAuctionInvalidated(auctionId);
+        } else {
+          // If the auction has ended, the highest bidder will be the new owner
+          // and if the auction is in progress, this will revert.
+
+          // `authorizeSeller != address(0)` does not apply here since an unsettled auction must go
+          // through this path to know who the authorized seller should be.
+          if (auction.bidder != authorizeSeller) {
+            revert NFTMarketReserveAuction_Not_Matching_Seller(auction.bidder);
+          }
+
+          // Finalization will revert if the auction has not yet ended.
+          _finalizeReserveAuction({ auctionId: auctionId });
+        }
+        // The seller authorization has been confirmed.
+        authorizeSeller = address(0);
+      }
+
+      IERC721(nftContract).transferFrom(address(this), recipient, tokenId);
+
+    }
+
+    function finalizeReserveAuction(uint256 auctionId) external nonReentrant {
+      if (auctionIdToAuction[auctionId].endTime == 0) {
+        revert NFTMarketReserveAuction_Cannot_Finalize_Already_Settled_Auction();
+      }
+      _finalizeReserveAuction({auctionId: auctionId});
+    }
+
+    function _finalizeReserveAuction(uint256 auctionId) private {
+      ReserveAuctionStorage storage auction = auctionIdToAuction[auctionId];
+
+      if (auction.endTime >= block.timestamp) {
+        revert NFTMarketReserveAuction_Cannot_Finalize_Auction_In_Progress(auction.endTime);
+      }
+
+      auction.finalized = true;
+      if (auction.bidder != address(0)) {
+        _transferFromEscrow(auction.nftContract, auction.tokenId, auction.bidder, address(0));
+        _distributeFunds(auction.payee1, auction.payee1Amount, auction.payee2, auction.amount);
+      } else {
+        _transferFromEscrow(auction.nftContract, auction.tokenId, auction.creator, address(0));
+      }
+
+      emit ReserveAuctionFinalized(auctionId, auction.payee1, auction.payee2, auction.creator, auction.bidder);
+    }
+
+    function _distributeFunds(address payee1, uint256 payee1Amount, address payee2, uint256 amount) internal {
+      uint256 amountToPayee1 = amount / 100 * payee1Amount;
+      uint256 amountToPayee2 = amount - amountToPayee1;
+      payable(payee1).transfer(amountToPayee1);
+      payable(payee2).transfer(amountToPayee2);
+    }
+
+    function placeBid(uint256 auctionId) public payable nonReentrant {
+      ReserveAuctionStorage storage auction = auctionIdToAuction[auctionId];
+      uint256 amount = msg.value;
+      if (auction.amount == 0) {
+        revert NFTMarketReserveAuction_Cannot_Bid_On_Nonexistent_Auction();
+      } else if (amount < msg.value) {
+        revert NFTMarketReserveAuction_Too_Much_Value_Provided();
+      }
+
+      uint256 endTime = auction.endTime;
+
+      if (endTime < block.timestamp) {
+        revert NFTMarketReserveAuction_Cannot_Bid_On_Ended_Auction(endTime);
+      } else if (auction.bidder == msg.sender) {
+        revert NFTMarketReserveAuction_Cannot_Rebid_Over_Outstanding_Bid();
+      } else {
+        uint256 minIncrement = _getMinIncrement(auction.amount);
+        if (amount < minIncrement) {
+          revert NFTMarketReserveAuction_Bid_Must_Be_At_Least_Min_Amount(minIncrement);
+        }
+      }
+
+      if (auction.numBids > 0) {
+        uint256 originalAmount = auction.amount;
+        address payable originalBidder = auction.bidder;
+        payable(originalBidder).transfer(originalAmount);
+      }
+
+      auction.amount = amount;
+      auction.bidder = payable(msg.sender);
+
+      unchecked {
+
+        uint256 endTimeWithExtension = block.timestamp + EXTENSION_DURATION;
+        if (endTime < endTimeWithExtension) {
+          endTime = endTimeWithExtension;
+          auction.endTime = endTime;
+        }
+      }
+
+      auction.numBids++;
+      bidders[auctionId].push(msg.sender);
+      bids[auctionId].bidder.push(msg.sender);
+      bids[auctionId].amount.push(msg.value);
+      bids[auctionId].timestamp.push(block.timestamp);
+      emit ReserveAuctionBidPlaced(auctionId, msg.sender, amount, endTime);
+    }
+
+    function _getMinIncrement(uint256 currentAmount) internal pure returns (uint256) {
+    uint256 minIncrement = currentAmount;
+    unchecked {
+      minIncrement /= MIN_PERCENT_INCREMENT_DENOMINATOR;
+    }
+    if (minIncrement == 0) {
+      // Since minIncrement reduces from the currentAmount, this cannot overflow.
+      // The next amount must be at least 1 wei greater than the current.
+      return currentAmount + 1;
+    }
+
+    return minIncrement + currentAmount;
+  }
+
+  function getAllBids(uint256 auctionId) public view returns (Bid memory) {
+    return bids[auctionId];
+  }
+
+  function withdraw() public onlyOwner {
+    payable(msg.sender).transfer(address(this).balance);
+  }
+}
