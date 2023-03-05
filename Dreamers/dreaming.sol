50dffe3b584c931c4d7d402c1958addc07620b6b
augustfr <68298953+augustfr@users.noreply.github.com>
2023-03-05 14:52:50 -0800

    add dreaming minter

---
+++ Dreamers/dreaming.sol
@@ -0,0 +1,193 @@
+/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMNOOXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXO0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMk,'lKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc.,kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMNo...cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc...lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMK:....cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc....:0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMk,.....cKMMMMMMMMMMMMMMMMMMMMMMMMMMWKc.....,kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNd.......cKWMMMMMMMMMMMMMMMMMMMMMMMKkc.......oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMKc........cKMMMMMMMMMMMMMMMMMMMMMMKc'........cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMO,.........cKMMMMMMMMMMMMMMMMMMMMKc..........,OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMWd'..........cKWMMMMMMMMMMMMMMMMMKl............dWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMXc...;;.......cKMMMMMMMMMMMMMMMMKc...'co,......cXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMO;..,x0:.......cKMMMMMMMMMMMMMMKl...cxKKc......;OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMWd'..;0W0:.......cKWMMMMMMMMMMMKc...cKWMNo......'xWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMXl...lXMW0:.......c0WMMMMMMMMMKl...cKMMMWx'......lXXkxxddddoddddxxxkO0KXXNWWWMMMMMMMMMMM
+MMMMMMMMMMMM0;...dWMMW0:.......c0WMMMMMMMKl...cKMMMMMO;......;0k,.'''',,''.......'',;::cxXMMMMMMMMMM
+MMMMMMMMMMMWx'..,kMMMMW0:.......c0WMMMMMKl...cKMMMMWWKc......'xXOO00KKKKK00Okdoc,'......cKMMMMMMMMMM
+MMMMMMMMMMMXl...:0MMMMMW0:.......:0WMMMKl...cKMMWKkdxKd.......oNMMMMMMMMMMMMMMMWXOd:'...lXMMMMMMMMMM
+MMMMMMMMMMM0:...lXMMMMMMWO:.......:0MMKl...cKMWKo,..;0k,......:KMMMMMMMMMMMMMMMMMMWNOc'.oNMMMMMMMMMM
+MMMMMMMMMMWx'..'dWMMMMMMMW0:.......:0Kl...cKMNk;....,k0:......,kMMMMMMMMMMMMMMMMMMMMMXl'oNMMMMMMMMMM
+MMMMMMMMMMNl...,OMMMMMMMMMW0:.......;;...cKWXo'......dKl.......oNMMMMMMMMMMMMMMMMMMMMM0:dWMMMMMMMMMM
+MMMMMMMMMM0:...:KMMMMMMMMMMW0c..........lKMNo'.......oXd'......cKMMMMMMMMMMMMMMMMMMMMMNKXMMMMMMMMMMM
+MMMMMMMMMWk,...lXMMMMMMMMMMMWKc........cKMWx,.......,kWk,......,OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMNo...'dWMMMMMMMMMMMMMKl......lKMMK:........:KMK:.......dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMWO;...'xWMMMMMMMMMMMMMMXl'...lKMMWx'........lXMXl.......;OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMNKOOd;.....;dkO0NMMMMMMMMMMMXo''lXMMMNo.........lNW0:........,lxkO0XWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMN0kkkkkkkkkkxxkOXWMMMMMMMMMMMN0ONMMMMNo.........lXWXOkkkkxxxxxxxxxk0WMXOkkkkkkkkkkkkkkkkkOXMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo.........:0MMMMMMMMMMMMMMMMMMMMN0OOxl,........,lxO0NMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx'........'xWMMMMMMMMMMMMMMMMMMMMMMMMNd'......'dNMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0:.........:KMMMMMMMMMMMMMMMMMMMMMMMMMk,......'xWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd'.........oXMMMMMMMMMMMMMMMMMMMMMMMMO,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXo.........'oXMMMMMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXo'.........c0WMMMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx;.........,dXWMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKo;'........;d0NMMMMMMMMMMMMMMMMMO,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKOxc'.......':dOKNWMMMMMMMMMMMWk,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0xl;'.......,:ldxkO00KK00Od:.......;OMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0kdl:;''.......''''''....',;cox0NMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0OkxxddddddddxxkkO0XNWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+// SPDX-License-Identifier: MIT
+// Contract authored by August Rosedale (@augustfr)
+// https://miragegallery.ai
+
+// TinyERC721 used (https://github.com/tajigen/tiny-erc721)
+
+pragma solidity ^0.8.19;
+
+import "./TinyERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import {RevokableOperatorFilterer} from "./RevokableOperatorFilterer.sol";
+import {RevokableDefaultOperatorFilterer} from "./RevokableDefaultOperatorFilterer.sol";
+import "./ERC2981Base.sol";
+
+contract dreaming is TinyERC721, RevokableDefaultOperatorFilterer, Ownable, ERC2981Base {
+    using Strings for uint256;
+    event PermanentURI(string _value, uint256 indexed _id);
+
+    RoyaltyInfo private _royalties;
+
+    struct Details {
+        string artworkName;
+        string artistName;
+        address artistAddress;
+        uint256 artistPercentage;
+        address payoutAddress;
+        string artworkDescription;
+        string metadataCID;
+        string imageCID;
+        uint256 artworkID;
+        bool detailsFrozen;
+    }
+
+    mapping(uint256 => Details) details;
+    mapping(string => uint256[]) nameToID;
+
+    string private baseURI = "ipfs://";
+    
+    constructor(string memory name, string memory symbol, address royaltyReceiver) TinyERC721(name, symbol, 0) {
+        _royalties = RoyaltyInfo(royaltyReceiver, uint24(1000));
+    }
+
+    function updateRoyalties(address recipient, uint256 value) public onlyOwner {
+        require(value <= 10000, "ERC2981Royalties: Too high");
+        _royalties = RoyaltyInfo(recipient, uint24(value));
+    }
+
+    function addArtwork(string memory artworkName, string memory artistName, uint256 artistPercentage, address payoutAddress, address artistAddress, string memory artworkDescription, string memory metadataCID, string memory imageCID) public onlyOwner {
+        uint256 artworkID = totalSupply();
+        details[artworkID].artworkName = artworkName;
+        details[artworkID].artistName = artistName;
+        details[artworkID].artistAddress = artistAddress;
+        details[artworkID].artistPercentage = artistPercentage;
+        details[artworkID].payoutAddress = payoutAddress;
+        details[artworkID].artworkDescription = artworkDescription;
+        details[artworkID].metadataCID = metadataCID;
+        details[artworkID].imageCID = imageCID;
+        details[artworkID].artworkID = totalSupply();
+        details[artworkID].detailsFrozen = false;
+        nameToID[artistName].push(artworkID);
+        _safeMint(msg.sender, 1, '');
+    }
+
+    function updateDetails(uint256 artworkID, string memory artworkName, string memory artistName, address artistAddress, uint256 artistPercentage, address payoutAddress, string memory artworkDescription, string memory metadataCID, string memory imageCID) public onlyOwner {
+        require(!details[artworkID].detailsFrozen, "Details are frozen for this artwork");
+        details[artworkID].artworkName = artworkName;
+        details[artworkID].artistName = artistName;
+        details[artworkID].artistAddress = artistAddress;
+        details[artworkID].artistPercentage = artistPercentage;
+        details[artworkID].payoutAddress = payoutAddress;
+        details[artworkID].artworkDescription = artworkDescription;
+        details[artworkID].metadataCID = metadataCID;
+        details[artworkID].imageCID = imageCID;
+    }
+
+    function getDetails(uint256 _artworkID) public view returns (string memory artworkName, string memory artistName, address artistAddress, uint256 artistPercentage, address payoutAddress, string memory artworkDescription, string memory metadataCID, string memory imageCID, uint256 artworkID, bool detailsFrozen) {
+        if (!_exists(_artworkID)) revert URIQueryForNonexistentToken();
+        artworkName = details[artworkID].artworkName;
+        artistName = details[artworkID].artistName;
+        artistAddress = details[artworkID].artistAddress;
+        artworkDescription = details[artworkID].artworkDescription;
+        artistPercentage = details[artworkID].artistPercentage;
+        payoutAddress = details[artworkID].payoutAddress;
+        metadataCID = details[artworkID].metadataCID;
+        imageCID = details[artworkID].imageCID;
+        artworkID = details[artworkID].artworkID;
+        detailsFrozen = details[artworkID].detailsFrozen;
+    }
+
+    function getIDbyArtist(string memory artistName) public view returns (uint256[] memory artworkIDs) {
+        return nameToID[artistName];
+    }
+
+    function freeze(uint256 artworkID) public onlyOwner {
+        require(!details[artworkID].detailsFrozen, "Already frozen");
+        details[artworkID].detailsFrozen = true;
+        string memory frozenURI = string.concat(baseURI, details[artworkID].metadataCID);
+        emit PermanentURI(string(abi.encodePacked(frozenURI)), artworkID);
+    }
+
+    function tokenURI(uint256 tokenID) public override view returns (string memory) {
+        if (!_exists(tokenID)) revert URIQueryForNonexistentToken();
+        return string.concat(baseURI,details[tokenID].metadataCID);
+    }
+
+    function withdraw() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function royaltyInfo(uint256, uint256 value) external view override returns (address receiver, uint256 royaltyAmount) {
+        RoyaltyInfo memory royalties = _royalties;
+        receiver = royalties.recipient;
+        royaltyAmount = (value * royalties.amount) / 10000;
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC2981Base, TinyERC721) returns (bool) { 
+        return interfaceId == type(IERC721).interfaceId || interfaceId == type(IERC721Metadata).interfaceId ||
+        super.supportsInterface(interfaceId) || interfaceId == type(IERC2981Royalties).interfaceId;
+    }
+
+    function setApprovalForAll(address operator, bool approved) public override onlyAllowedOperatorApproval(operator) {
+        super.setApprovalForAll(operator, approved);
+    }
+
+    function approve(address operator, uint256 tokenId) public override onlyAllowedOperatorApproval(operator) {
+        super.approve(operator, tokenId);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+
+    function owner() public view virtual override (Ownable, RevokableOperatorFilterer) returns (address) {
+        return Ownable.owner();
+    }
+}
