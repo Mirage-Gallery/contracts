3a00140d988b85de405cb0de2cd5bb92c52fb748
augustfr <august@rosedales.com>
2024-02-21 09:33:22 -0800

    contracts for new curated method

---
+++ Curated/curatedIndividualTemplate.sol
@@ -0,0 +1,409 @@
+/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMNOOXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXO0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMk,'lKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc.,kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMNo...cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc...lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMK:....cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKc....:0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMk,.....cKMMMMMMMMMMMMMMMMMMMMMMMMMMWKc.....,kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNd.......cKWMMMMMMMMMMMMMMMMMMMMMMMKkc.......oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMKc........cKMMMMMMMMMMMMMMMMMMMMMMKc'........cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMO,.........cKMMMMMMMMMMMMMMMMMMMMKc..........,OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMWd'..........cKWMMMMMMMMMMMMMMMMMKl............dWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMXc...;;.......cKMMMMMMMMMMMMMMMMKc...'co,......cXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMO;..,x0:.......cKMMMMMMMMMMMMMMKl...cxKKc......;OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMWd'..;0W0:.......cKWMMMMMMMMMMMKc...cKWMNo......'xWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMXl...lXMW0:.......c0WMMMMMMMMMKl...cKMMMWx'......lXXkxxddddoddddxxxkO0KXXNWWWMMMMMMMMMMM
+MMMMMMMMMMMM0;...dWMMW0:.......c0WMMMMMMMKl...cKMMMMMO;......;0k,.'''',,''.......'',;::cxXMMMMMMMMMM
+MMMMMMMMMMMWx'..,kMMMMW0:.......c0WMMMMMKl...cKMMMMWWKc......'xXOO00KKKKK00Okdoc,'......cKMMMMMMMMMM
+MMMMMMMMMMMXl...:0MMMMMW0:.......:0WMMMKl...cKMMWKkdxKd.......oNMMMMMMMMMMMMMMMWXOd:'...lXMMMMMMMMMM
+MMMMMMMMMMM0:...lXMMMMMMWO:.......:0MMKl...cKMWKo,..;0k,......:KMMMMMMMMMMMMMMMMMMWNOc'.oNMMMMMMMMMM
+MMMMMMMMMMWx'..'dWMMMMMMMW0:.......:0Kl...cKMNk;....,k0:......,kMMMMMMMMMMMMMMMMMMMMMXl'oNMMMMMMMMMM
+MMMMMMMMMMNl...,OMMMMMMMMMW0:.......;;...cKWXo'......dKl.......oNMMMMMMMMMMMMMMMMMMMMM0:dWMMMMMMMMMM
+MMMMMMMMMM0:...:KMMMMMMMMMMW0c..........lKMNo'.......oXd'......cKMMMMMMMMMMMMMMMMMMMMMNKXMMMMMMMMMMM
+MMMMMMMMMWk,...lXMMMMMMMMMMMWKc........cKMWx,.......,kWk,......,OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMNo...'dWMMMMMMMMMMMMMKl......lKMMK:........:KMK:.......dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMWO;...'xWMMMMMMMMMMMMMMXl'...lKMMWx'........lXMXl.......;OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMNKOOd;.....;dkO0NMMMMMMMMMMMXo''lXMMMNo.........lNW0:........,lxkO0XWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMN0kkkkkkkkkkxxkOXWMMMMMMMMMMMN0ONMMMMNo.........lXWXOkkkkxxxxxxxxxk0WMXOkkkkkkkkkkkkkkkkkOXMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo.........:0MMMMMMMMMMMMMMMMMMMMN0OOxl,........,lxO0NMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWx'........'xWMMMMMMMMMMMMMMMMMMMMMMMMNd'......'dNMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0:.........:KMMMMMMMMMMMMMMMMMMMMMMMMMk,......'xWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd'.........oXMMMMMMMMMMMMMMMMMMMMMMMMO,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXo.........'oXMMMMMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXo'.........c0WMMMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNx;.........,dXWMMMMMMMMMMMMMMMMMMMO,......'kMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKo;'........;d0NMMMMMMMMMMMMMMMMMO,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKOxc'.......':dOKNWMMMMMMMMMMMWk,......'kWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0xl;'.......,:ldxkO00KK00Od:.......;OMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0kdl:;''.......''''''....',;cox0NMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK0OkxxddddddddxxkkO0XNWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+
+// SPDX-License-Identifier: MIT
+// Contract authored by August Rosedale (@augustfr)
+// https://miragegallery.ai
+
+pragma solidity ^0.8.24;
+
+import "./tinyERC721_ID.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
+import "./IDelegationRegistry.sol";
+
+interface mirageContracts {
+    function balanceOf(
+        address owner,
+        uint256 _id
+    ) external view returns (uint256);
+}
+
+contract mirageCuratedTemplate is
+    TinyERC721,
+    ReentrancyGuard,
+    Ownable,
+    IERC2981
+{
+    using Strings for uint256;
+
+    mapping(uint256 => uint256) public sentientClaimed;
+
+    IDelegateRegistry public immutable registry;
+
+    uint256 private miragePercentage = 15;
+    address private mirageAddress = 0x4a462cbf4902A8D26bF2c6931F12260d8fcEC69e;
+
+    uint256 private royaltyAmount = 1000;
+    address private royaltyAddress = 0xdCB23900d19b808F5510E8bB84E9B6988a797bb2;
+
+    bool public metadataFrozen;
+
+    bool public presaleOpen = false;
+    bool public publicSaleOpen = false;
+    bool public sentientClaimOpen = false;
+
+    uint256 public maxPerPublicTx = 10;
+
+    address public admin;
+
+    modifier onlyAdmin() {
+        require(
+            msg.sender == admin || msg.sender == owner(),
+            "Only admin or owner"
+        );
+        _;
+    }
+
+    struct ProjectDetails {
+        uint256 artworks;
+        string artistName;
+        string projectDescription;
+        string projectName;
+        address artistAddress;
+        string baseURI;
+        string projectWebsite;
+        string projectLicense;
+        uint256 maxSupply;
+        uint256 maxPresale;
+        uint256 mintPrice;
+        address additionalPayeeAddress;
+        uint256 additionalPayeePercentage;
+    }
+
+    ProjectDetails public projectDetails;
+
+    mirageContracts public membershipContract;
+    address private membershipAddress;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address _registry,
+        address _membershipAddress
+    ) TinyERC721(name, symbol, 0) {
+        membershipContract = mirageContracts(_membershipAddress);
+        membershipAddress = _membershipAddress;
+        admin = msg.sender;
+        registry = IDelegateRegistry(_registry);
+    }
+
+    function addProjectDetails(
+        uint256 _mintPrice,
+        string memory _artistName,
+        string memory _projectDescription,
+        string memory _projectName,
+        address _artistAddress,
+        string memory _baseURI,
+        string memory _projectWebsite,
+        string memory _projectLicense,
+        uint256 _maxSupply,
+        uint256 _maxPresale
+    ) public onlyAdmin {
+        projectDetails.artworks = 50;
+        projectDetails.mintPrice = _mintPrice;
+        projectDetails.artistName = _artistName;
+        projectDetails.projectDescription = _projectDescription;
+        projectDetails.projectName = _projectName;
+        projectDetails.artistAddress = _artistAddress;
+        projectDetails.baseURI = _baseURI;
+        projectDetails.projectWebsite = _projectWebsite;
+        projectDetails.projectLicense = _projectLicense;
+        projectDetails.maxSupply = _maxSupply;
+        projectDetails.maxPresale = _maxPresale;
+    }
+
+    function updateAdminAddress(address _newAdmin) public onlyAdmin {
+        admin = _newAdmin;
+    }
+
+    function setAdditionalPayee(
+        address _additionalPayee,
+        uint256 _additionalPayeePercentage
+    ) external onlyAdmin {
+        require(_additionalPayee != address(0), "Invalid address");
+        require(
+            _additionalPayeePercentage <= 100,
+            "Percentage cannot exceed 100"
+        );
+        projectDetails.additionalPayeeAddress = _additionalPayee;
+        projectDetails.additionalPayeePercentage = _additionalPayeePercentage;
+    }
+
+    function projectTokenInfo()
+        public
+        view
+        returns (
+            address artistAddress,
+            uint256 pricePerTokenInWei,
+            uint256 artworks,
+            uint256 maxArtworks,
+            uint256 maxEarly,
+            address additionalPayee,
+            uint256 additionalPayeePercentage,
+            bool publicActive,
+            bool earlyActive,
+            string memory currency
+        )
+    {
+        artistAddress = projectDetails.artistAddress;
+        pricePerTokenInWei = projectDetails.mintPrice;
+        artworks = projectDetails.artworks;
+        maxArtworks = projectDetails.maxSupply;
+        maxEarly = projectDetails.maxPresale;
+        additionalPayee = projectDetails.additionalPayeeAddress;
+        additionalPayeePercentage = projectDetails.additionalPayeePercentage;
+        publicActive = publicSaleOpen;
+        earlyActive = presaleOpen;
+        currency = "ETH";
+    }
+
+    function updateMirageAddress(address _newMirageAddress) public onlyAdmin {
+        require(_newMirageAddress != address(0), "Invalid address");
+        mirageAddress = _newMirageAddress;
+    }
+
+    function updateMaxPerPublicTx(uint256 _newAmount) public onlyAdmin {
+        maxPerPublicTx = _newAmount;
+    }
+
+    function updateProjectDetails(
+        uint256 _mintPrice,
+        string memory _artistName,
+        string memory _projectDescription,
+        string memory _projectName,
+        address _artistAddress,
+        string memory _baseURI,
+        string memory _projectWebsite,
+        string memory _projectLicense,
+        uint256 _maxSupply,
+        uint256 _maxPresale,
+        address _additionalPayeeAddress,
+        uint256 _additionalPayeePercentage
+    ) public onlyAdmin {
+        require(!metadataFrozen, "Metadata is frozen");
+        projectDetails.mintPrice = _mintPrice;
+        projectDetails.artistName = _artistName;
+        projectDetails.projectDescription = _projectDescription;
+        projectDetails.projectName = _projectName;
+        projectDetails.artistAddress = _artistAddress;
+        projectDetails.baseURI = _baseURI;
+        projectDetails.projectWebsite = _projectWebsite;
+        projectDetails.projectLicense = _projectLicense;
+        projectDetails.maxSupply = _maxSupply;
+        projectDetails.maxPresale = _maxPresale;
+        projectDetails.additionalPayeeAddress = _additionalPayeeAddress;
+        projectDetails.additionalPayeePercentage = _additionalPayeePercentage;
+    }
+
+    function togglePresale() public onlyAdmin {
+        presaleOpen = !presaleOpen;
+        sentientClaimOpen = true;
+    }
+
+    function togglePublicSale() public onlyAdmin {
+        publicSaleOpen = !publicSaleOpen;
+        sentientClaimOpen = true;
+        presaleOpen = false;
+    }
+
+    function earlyMemberPurchase(
+        uint256 _membershipId,
+        address _vault
+    ) public payable nonReentrant {
+        require(
+            msg.value >= projectDetails.mintPrice,
+            "Ether sent is insufficient"
+        );
+        require(
+            projectDetails.artworks - 50 < projectDetails.maxPresale,
+            "Max supply exceeded"
+        );
+        require(presaleOpen, "Presale is not open");
+
+        address requester = _vault == msg.sender ? msg.sender : _vault;
+        bool isValidDelegate = _vault == msg.sender
+            ? true
+            : registry.checkDelegateForContract(
+                msg.sender,
+                _vault,
+                membershipAddress,
+                ""
+            );
+        require(isValidDelegate, "Invalid delegate-vault pairing");
+        require(
+            membershipContract.balanceOf(requester, _membershipId) > 0,
+            "Membership token missing"
+        );
+
+        _splitFunds(1);
+        projectDetails.artworks += 1;
+        _safeMint(requester, 1);
+    }
+
+    function purchase(uint256 numberOfTokens) public payable nonReentrant {
+        require(publicSaleOpen, "Public sale is not open");
+        require(
+            numberOfTokens <= maxPerPublicTx,
+            "Exceeds max tokens per transaction"
+        );
+        require(
+            msg.value >= projectDetails.mintPrice * numberOfTokens,
+            "Insufficient payment"
+        );
+        require(
+            msg.sender == tx.origin,
+            "Calls must be made directly by a user"
+        );
+        require(
+            projectDetails.artworks + numberOfTokens <=
+                projectDetails.maxSupply,
+            "Would exceed max supply"
+        );
+
+        _splitFunds(numberOfTokens);
+        projectDetails.artworks += numberOfTokens;
+        _safeMint(msg.sender, numberOfTokens);
+    }
+
+    function claimSentient(uint256 membershipId, address _vault) public {
+        require(membershipId < 50, "Invalid Sentient ID");
+        require(sentientClaimOpen, "Sentient claim is not open");
+
+        address requester = _vault == msg.sender ? msg.sender : _vault;
+        bool isValidDelegate = _vault == msg.sender
+            ? true
+            : registry.checkDelegateForContract(
+                msg.sender,
+                _vault,
+                membershipAddress,
+                ""
+            );
+        require(isValidDelegate, "Invalid delegate-vault pairing");
+        require(
+            membershipContract.balanceOf(requester, membershipId) > 0,
+            "Membership token missing"
+        );
+
+        _safeMintID(requester, membershipId);
+    }
+
+    function freezeMetadata() public onlyAdmin {
+        require(!metadataFrozen, "Already frozen");
+        metadataFrozen = true;
+    }
+
+    function tokenURI(
+        uint256 tokenID
+    ) public view override returns (string memory) {
+        if (!_exists(tokenID)) revert URIQueryForNonexistentToken();
+        return string.concat(projectDetails.baseURI, Strings.toString(tokenID));
+    }
+
+    function updateURI(string memory _baseTokenURI) external onlyAdmin {
+        require(!metadataFrozen, "Metadata is frozen");
+        projectDetails.baseURI = _baseTokenURI;
+    }
+
+    function withdraw() public onlyAdmin {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function _splitFunds(uint256 numberOfTokens) internal {
+        if (msg.value == 0) return;
+
+        uint256 mintCost = projectDetails.mintPrice * numberOfTokens;
+        uint256 refund = msg.value - mintCost;
+        if (refund > 0) {
+            payable(msg.sender).transfer(refund);
+        }
+
+        uint256 mirageAmount = (mintCost * miragePercentage) / 100;
+        if (mirageAmount > 0) {
+            payable(mirageAddress).transfer(mirageAmount);
+        }
+
+        uint256 remainingFunds = mintCost - mirageAmount;
+        uint256 additionalPayeeAmount = 0;
+        if (projectDetails.additionalPayeePercentage > 0) {
+            additionalPayeeAmount =
+                (remainingFunds * projectDetails.additionalPayeePercentage) /
+                100;
+            if (additionalPayeeAmount > 0) {
+                payable(projectDetails.additionalPayeeAddress).transfer(
+                    additionalPayeeAmount
+                );
+            }
+        }
+
+        uint256 creatorFunds = remainingFunds - additionalPayeeAmount;
+        if (creatorFunds > 0) {
+            payable(projectDetails.artistAddress).transfer(creatorFunds);
+        }
+    }
+
+    function royaltyInfo(
+        uint256 /* tokenId */,
+        uint256 salePrice
+    ) external view override returns (address receiver, uint256 royaltyFee) {
+        uint256 totalRoyalty = (salePrice * royaltyAmount) / 10000;
+
+        return (royaltyAddress, totalRoyalty);
+    }
+
+    function updateRoyaltyInfo(
+        address _royaltyAddress,
+        uint256 _royaltyAmount
+    ) external onlyAdmin {
+        require(_royaltyAmount <= 10000, "Royalty cannot exceed 100%");
+        royaltyAddress = _royaltyAddress;
+        royaltyAmount = _royaltyAmount;
+    }
+}
